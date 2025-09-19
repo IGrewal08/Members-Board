45501c368993b4483146636c98c07f22dfefce0e
Inder Grewal <inderdeepg8@gmail.com>
2025-09-18 20:24:56 -0700

    created template for header and footer, added populate module to build db

--- config/populate.config.js
+++ config/populate.config.js
@@ -1,10 +1,58 @@
-import { Pool } from "pg";
+import { Client } from "pg";
+import "dotenv/config";
 
 const create_tables = `
+CREATE EXTENSION citext;
+
 CREATE TABLE IF NOT EXISTS users (
+userId INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+email citext UNIQUE NOT NULL,
+name VARCHAR(100) NOT NULL,
+password VARCHAR(100) NOT NULL,
+isAdmin BOOLEAN DEFAULT FALSE
 );
+
 CREATE TABLE IF NOT EXISTS posts (
+postId INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+userId INT NOT NULL REFERENCES users(userId),
+title VARCHAR(100) NOT NULL,
+description TEXT NOT NULL,
+likes INT NOT NULL DEFAULT 0,
+date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-CREATE TABLE IF NOT EXISTS messages (
+
+CREATE TABLE IF NOT EXISTS comments (
+commentId INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+parent_commentId INT REFERENCES comments(commentId),
+userId INT NOT NULL REFERENCES users(userId),
+comment VARCHAR(500) NOT NULL,
+date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS post_likes (
+userId INT NOT NULL REFERENCES users(userId),
+postId INT NOT NULL REFERENCES posts(postId),
+time_liked TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (userId, postId)
 );
 `;
+
+const seed = `
+INSERT INTO users (email, name, password, isAdmin) VALUES ('example@gmail.com', 'John Smith', '1234', true);
+INSERT INTO posts (userId, title, description, likes) VALUES (1, 'Sample Title', 'This is the sample description for this sample post', 1);
+INSERT INTO comments (userId, comment) VALUES (1, 'This is the sample comment on the sample post');
+INSERT INTO post_likes (userId, postId) VALUES (1, 1);
+`;
+
+async function main() {
+  console.log("Seeding...");
+  const client = new Client({
+    connectionString: `postgresql://${process.env.ROLE_NAME}@localhost:${process.env.PORT}/${process.env.DATABASE_NAME}`,
+  });
+  await client.connect();
+  client.query(create_tables);
+  client.query(seed);
+  client.end();
+  console.log("Seeding Complete");
+}
+main();
--- routes/signup.routes.js
+++ routes/signup.routes.js
@@ -4,4 +4,17 @@ const signupRouter = Router();
 
 signupRouter("/sign-up", (req, res) => res.redirect("signup.ejs"));
 
+/* 
+[
+    // password must be at least 8 characters long
+    body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+    // password must contain at least one uppercase letter
+    body('password').matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter'),
+    // password must contain at least one number
+    body('password').matches(/[0-9]/).withMessage('Password must contain at least one number'),
+    // password must contain at least one special character
+    body('password').matches(/[\W_]/).withMessage('Password must contain at least one special character')
+  ];
+*/
+
 export default signupRouter;
