4c51257b5ef6e86f439be169067c57134764ffe2
Inder Grewal <inderdeepg8@gmail.com>
2025-09-20 23:09:25 -0700

    modified home path with ejs

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,27 +1,37 @@
 import { body, query, validationResult } from "express-validator";
 
 export const getHome = async (req, res) => {
-    //console.log(req.headers);
-    try {
-        res.render("index", {
-            title: "Home",
-        });
-    } catch (error) {
-        console.error("Error in getHome controller: ", error);
-    }
-}
+  //console.log(req.headers);
+  const now = new Date();
+  try {
+    res.render("index", {
+      likedPosts: '',
+      myPosts: '',
+      query: '',
+      time: `${
+        now.getMonth() + 1
+      }/${now.getDate()}/${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`,
+    });
+  } catch (error) {
+    console.error("Error in getHome controller: ", error);
+  }
+};
 
 export const getPostForm = (req, res) => {
-    res.redirect();
-}
+  res.redirect();
+};
+
+export const postPostForm = (req, res) => {
+
+};
 
 export const getPost = async (req, res) => {
-    try {
-        res.render("post.ejs", {
-            title: "",
-            post: "",
-        });
-    } catch (error) {
-        console.error("Error in getPost controller: ", error);
-    }
-}
+  try {
+    res.render("post.ejs", {
+      title: "",
+      post: "",
+    });
+  } catch (error) {
+    console.error("Error in getPost controller: ", error);
+  }
+};
